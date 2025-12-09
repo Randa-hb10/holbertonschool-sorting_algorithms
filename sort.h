8f9932141befcffdbacd1d4d7257d876cfbcc8ee
Randa-hb10 <rm-400000@hotmail.com>
2025-12-09 10:10:31 +0000

    Task 2: Selection sort implementation

--- sort.h
+++ sort.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 /* Function prototypes for print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void selection_sort(int *array, size_t size);
 
 /* Sorting algorithms prototypes */
 void bubble_sort(int *array, size_t size);
