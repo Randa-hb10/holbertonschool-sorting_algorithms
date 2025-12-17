40603d802b0f03ca93909721262bd5602a30d895
maryam13188 <roro13188@gmail.com>
2025-12-17 08:47:38 +0000

    fix: Add missing shell_sort prototype for task 4

--- sort.h
+++ sort.h
@@ -2,34 +2,30 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+        const int n;
+        struct listint_s *prev;
+        struct listint_s *next;
 } listint_t;
 
-/* Function prototypes for print functions */
+/* Function prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void selection_sort(int *array, size_t size);
 
 /* Sorting algorithms prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);  /* أضف هذا السطر هنا! */
 
-/* Helper functions for sorting algorithms */
+/* Helper functions */
 void swap_ints(int *a, int *b);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 int lomuto_partition(int *array, int low, int high, size_t size);
